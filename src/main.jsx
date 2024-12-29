b350fae505ad040e2422942e9f6ad7010a558da1
MINGI <sssky1561@naver.com>
2024-12-29 13:44:50 +0900

    rewrites commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  // createHashRouter,
+} from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import './index.css';
